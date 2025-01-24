638ec3c060dd61eb365e38144e902c0b78b5cee6
dejvidEm <bithug10@gmail.com>
2025-01-24 23:41:37 +0100

    mobile course icon update

--- src/components/ui/ArrowButtonCourse.tsx
+++ src/components/ui/ArrowButtonCourse.tsx
@@ -3,16 +3,21 @@ import React from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
 import { LuCalendarDays } from "react-icons/lu";
 
-
-
 type AnimatedButtonProps = {
   text: string;
   variant?: "green" | "white";
 };
 
-const ArrowButtonCourse: React.FC<AnimatedButtonProps> = ({ text, variant = "green" }) => {
-    const backgroundClass = variant === "green" ? "bg-[#F3F3F1]" : "bg-white";
-    const textColorClass = variant === "green" ? "text-gray-800 hover:text-white transition duration-300" : "hover:text-white transition duration-300";
+const ArrowButtonCourse: React.FC<AnimatedButtonProps> = ({
+  text,
+  variant = "green",
+}) => {
+  const backgroundClass =
+    variant === "green" ? "bg-[#F3F3F1]" : "bg-white";
+  const textColorClass =
+    variant === "green"
+      ? "text-gray-800 hover:text-white transition duration-300"
+      : "hover:text-white transition duration-300";
 
   return (
     <button
@@ -29,9 +34,16 @@ const ArrowButtonCourse: React.FC<AnimatedButtonProps> = ({ text, variant = "gre
                  hover:before:scale-x-100
                  ${textColorClass}`}
     >
-    <LuCalendarDays className="z-20 mr-4"/>
-      <span className={`relative text-sm md:text-base -ml-16`}>{text}</span>
-      <MdOutlineArrowOutward size={22} className="z-10 text-slate-50"/>
+      <LuCalendarDays className="z-20 mr-4 flex-shrink-0 w-6 h-6" />
+      <span
+        className={`relative text-sm md:text-base text-center ml-auto mr-auto`}
+      >
+        {text}
+      </span>
+      <MdOutlineArrowOutward
+        size={20}
+        className="z-10 text-slate-50 flex-shrink-0"
+      />
     </button>
   );
 };
